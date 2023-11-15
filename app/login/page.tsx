9b88acf347ba965cc72b7685a0c0d8af0065de60
sammit badodekar <badodekarsammit@gmail.com>
2023-11-15 10:40:46 +0530

    test4: fix 307 after login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,17 @@
-"use client";
-
 import { LoginForm } from "@/components/forms/loginForm";
 import React from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
-  const { data: session } = useSession();
-  const router = useRouter();
+const LoginPage = async () => {
+  const session = await getServerSession();
 
   console.log(session?.user);
 
-  /* if (session?.user) {
-    if (session?.user?.name) 
-    if (!session?.user?.name) 
-  } */
+  if (session?.user) {
+    if (session?.user?.name) redirect("/");
+    if (!session?.user?.name) redirect("/setup-profile");
+  }
 
   return (
     <div className=" flex h-[100dvh] w-full items-center justify-center">
