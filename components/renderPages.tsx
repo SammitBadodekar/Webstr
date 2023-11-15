2ed9962e5eca594551a10277a9991dd9d1116797
sammit badodekar <badodekarsammit@gmail.com>
2023-11-15 10:20:46 +0530

    test3: status 307 after login

---
+++ components/renderPages.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RenderPages = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default RenderPages;
--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,16 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
-import { appMiddleware } from "./lib/middleware/app";
+import { withAuth } from "next-auth/middleware";
 
-export const config = {
-  matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api/ routes
-     * 2. /_next/ (Next.js internals)
-     * 3. /_proxy/, /_auth/ (special pages for OG tags proxying and password protection)
-     * 4. /_static (inside /public)
-     * 5. /_vercel (Vercel internals)
-     * 6. /favicon.ico, /sitemap.xml (static files)
-     */
-    "/((?!api/|login|sw.js|_next/|_proxy/|_auth/|_static|_vercel|favicon.ico|sitemap.xml).*)",
-  ],
-};
-
-export default async function middleware(req: NextRequest) {
-  return appMiddleware(req);
-}
+export default withAuth(
+  function middleware(req) {
+    console.log(req.nextauth.token);
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => (token?.email ? true : false),
+    },
+    pages: {
+      signIn: "/login",
+      error: "/error",
+    },
+  }
+);
