ab1bb40a7d0cd518f84e9fed2704b9a76308da93
Turbobot <turbobot@vercel.com>
2023-12-05 10:52:25 +0530

    feat(create-turbo): apply official-starter transform

--- components/elements/text.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Text = () => {
-  return <div>Text</div>;
-};
-
-export default Text;
--- components/uiRenderer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Render = ({ data }: { data: any[] }) => {
-  return <div>Render</div>;
-};
-
-export default Render;
