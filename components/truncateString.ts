07a29e65ae710399ddde206b5ff921855237a151
sammit badodekar <badodekarsammit@gmail.com>
2023-12-27 12:57:56 +0530

    fix: multiple page.tsx error during build

--- components/truncateString.ts
+++
@@ -1,8 +0,0 @@
-export function truncateString(name: string, maxWidth: number) {
-  if (name?.length * 10 > maxWidth) {
-    // assume ~10px per char
-    const charCount = Math.floor(maxWidth / 10) - 1;
-    return `${name?.substring(0, charCount)}...`;
-  }
-  return name;
-}
